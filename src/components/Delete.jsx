27c560e6a502348f3ebea2f10004a0c6d971953d
DippinDot <jpmarino33@gmail.com>
2022-07-06 15:35:02 -0600

    more notes for tomorrow

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -14,3 +14,11 @@ export default function Delete() {
 //double check props passed into deletepost is correct
 
 //we should make it so the delete and edit button only show when the userid===post id
+
+//Your posts should have a way for the active user to delete them,
+// only if isAuthor is true for the post. Go ahead and add that now.
+//You'll also have to add a click handler to make that DELETE request.
+//For each post, the delete handler will need a way to recover the post._id to help
+// form the correct URL for the request.
+//On successful delete, make sure to remove that post from the page
+// as well as from the array that is holding all posts in state.
